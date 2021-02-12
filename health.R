e5be7915521779562f19bc52912b29f9d89d204d
Conor <conor@Conors-MacBook-Air.local>
2021-02-11 17:14:34 -0800

    added data

---
+++ health.R
@@ -0,0 +1,11 @@
+# Does ___ affect ___?
+# This program leverages the fact that several states opted to expand medicaid years after healtcare quality
+# data began being published in 2014 to construct a difference-in-differences analysis of the effects of 
+# the medicare expansion on the amount of time 
+# Conor Hennessy
+# February, 2021
+
+#visualization of summary statistics
+
+
+#quasiexperimental (d-in-d) regression model
--- health2.R
+++
@@ -1,4 +0,0 @@
-# Does medicaid expansion reduce the amount of time spent on unpaid home health care?
-# A difference-in-differences analysis
-# Conor Hennessy
-# February, 2021
